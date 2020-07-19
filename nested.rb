3c5e1d6b864b63df85806e4438bc5e958e8eceac
Justin Webb <webb.n.justin@gmail.com>
2020-07-19 00:55:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,6 +132,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-  programmer_hash[languages] = "C, Assembley" 
+  programmer_hash[:dennis_ritchie][:languages] = "C, Assembley" 
 return programmer_hash
 end
